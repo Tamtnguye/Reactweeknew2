11eeb7199bd5c157378a26b711f6701abb9f43be
Tamtnguye <apple@Apples-MacBook-Pro.local>
2020-09-22 17:15:15 -0700

    components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import Todo from "./components/Todo";
+import Forms from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
 
 function App(props) {
+  const taskList = props.tasks.map(task => (
+  <Todo 
+  id={task.id} 
+  name={task.name} 
+  completed={task.completed} 
+  key={task.id}
+  />
+  ));
   return (
+    
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
+      <Forms />
+        {/* <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
           </label>
@@ -21,10 +32,13 @@ function App(props) {
         />
         <button type="submit" className="btn btn__primary btn__lg">
           Add
-        </button>
-      </form>
+        </button> */}
+      
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+        {/* <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show </span>
           <span>all</span>
           <span className="visually-hidden"> tasks</span>
@@ -38,7 +52,7 @@ function App(props) {
           <span className="visually-hidden">Show </span>
           <span>Completed</span>
           <span className="visually-hidden"> tasks</span>
-        </button>
+        </button> */}
       </div>
       <h2 id="list-heading">
         3 tasks remaining
@@ -48,7 +62,9 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <li className="todo stack-small">
+        {taskList}
+        {/* everything commented here is replaced with that taskList. saving so much coding 
+        <Todo name="Eat" completed={true} id="todo-0" className="todo stack-small">
           <div className="c-cb">
             <input id="todo-0" type="checkbox" defaultChecked={true} />
             <label className="todo-label" htmlFor="todo-0">
@@ -63,8 +79,8 @@ function App(props) {
               Delete <span className="visually-hidden">Eat</span>
             </button>
           </div>
-        </li>
-        <li className="todo stack-small">
+        </Todo>
+        <Todo completed={false} name="Sleep" id="todo-1" className="todo stack-small">
           <div className="c-cb">
             <input id="todo-1" type="checkbox" />
             <label className="todo-label" htmlFor="todo-1">
@@ -79,8 +95,8 @@ function App(props) {
               Delete <span className="visually-hidden">Sleep</span>
             </button>
           </div>
-        </li>
-        <li className="todo stack-small">
+        </Todo>
+        <Todo name="Repeat" completed={false} id="todo-2" className="todo stack-small">
           <div className="c-cb">
             <input id="todo-2" type="checkbox" />
             <label className="todo-label" htmlFor="todo-2">
@@ -95,7 +111,7 @@ function App(props) {
               Delete <span className="visually-hidden">Repeat</span>
             </button>
           </div>
-        </li>
+        </Todo> */}
       </ul>
     </div>
   );
