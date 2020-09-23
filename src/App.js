dacaa648dbf11235b67c502761f6964740a7d5bd
Tamtnguye <apple@Apples-MacBook-Pro.local>
2020-09-22 19:10:47 -0700

    add,delete works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Forms from "./components/Form";
+import { nanoid } from "nanoid";
 import FilterButton from "./components/FilterButton";
 
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  
+
+  const [tasks, setTasks] = useState(props.tasks);
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //use object spread to make a new object
+        //whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  const taskList = tasks.map(task => (
   <Todo 
   id={task.id} 
   name={task.name} 
   completed={task.completed} 
   key={task.id}
+  toggleTaskCompleted={toggleTaskCompleted}
+  deleteTask={deleteTask}
   />
   ));
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText =`${taskList.length} ${tasksNoun} remaining`;
   return (
     
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Forms />
+      <Forms   addTask={addTask}/>
         {/* <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
@@ -55,7 +83,7 @@ function App(props) {
         </button> */}
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
